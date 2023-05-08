b77471c4e44d72f9800fde92f2468990bdca9f4c
DeboraMed <dmedina@iesrafaelalberti.es>
2023-05-08 12:02:21 +0200

    Tablas creadas y conexion con la BBDD

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,6 @@
 import java.sql.DriverManager
 import java.sql.ResultSet
+import java.sql.SQLException
 import java.sql.Statement
 
 val jdbcUrl = "jdbc:h2:./db" // ruta relativa
@@ -30,40 +31,199 @@ fun main(args: Array<String>) {
     println(mejoresCtfByGroupId)
      */
 
+    //mi codigo
+    var ctf=Ctf(3,3,342344)
+    anadeParticipacion(ctf)
+
+    val grupoid=3
+    val ctfid=3
+    eliminaParticipacion(grupoid,ctfid)
+
 }
 // 1º comando añade participacion de un grupo (insert en ctf) consulta sobre ctf con mejor puntucion de ese grupo
 //(update) de mejor posicion
-fun insertaGrupo(grupos:Grupo){
+fun anadeParticipacion(ctf:Ctf){
 
-    //conexion con la BBDD
-    val conn = DriverManager.getConnection(jdbcUrl, userName, password)
-    //sentencia insert
-    val insertSqlString = ("insert into grupos(grupoid, grupodesc) values(?,'?')")
+    try {
 
-    var preparedStatement = conn.prepareStatement(insertSqlString) // Uso de PreparedStatement
+        //conexion con la BBDD
+        val conn = DriverManager.getConnection(jdbcUrl, userName, password)
+        //sentencia insert
+        val insertSqlString = ("INSERT INTO CTFS(CTFid, grupoid, puntuacion) VALUES(?,?,?)")
+        //dado un equipo, el ID del CTF donde hizo mas puntos
 
-    preparedStatement.setInt(1, grupos.grupoid)
-    preparedStatement.setInt(2, grupos.mejorCtfId)
 
-    val insertCount = preparedStatement.executeUpdate()
+        var preparedStatement = conn.prepareStatement(insertSqlString) // Uso de PreparedStatement
 
-    //comprueba si se ha realizado el insert
-    if (insertCount > 0) {
-        println("Se ha insertado la fila correctamente")
-    } else {
-        println("La inserción de fila ha fallado")
+        preparedStatement.setInt(1, ctf.id)
+        preparedStatement.setInt(2, ctf.grupoId)
+        preparedStatement.setInt(3, ctf.puntuacion)
+
+        //ejecuta el insert
+        val insertCount = preparedStatement.executeUpdate()
+
+
+        val updateSqlString = ("UPDATE GRUPOS\n" +
+                "SET mejorposCTFid = (\n" +
+                "  select ctfid\n" +
+                "  from ctfs\n" +
+                "  where grupoid=? and puntuacion=(\n" +
+                "    select max(puntuacion)\n" +
+                "    from CTFS\n" +
+                "    where grupoid=?\n" +
+                "  )\n" +
+                "  limit 1\n" +
+                ")\n" +
+                "where grupoid=?;")
+
+
+        preparedStatement = conn.prepareStatement(updateSqlString) // Uso de PreparedStatement
+
+        preparedStatement.setInt(1, ctf.grupoId)
+        preparedStatement.setInt(2, ctf.grupoId)
+        preparedStatement.setInt(3, ctf.grupoId)
+
+        //ejecuta el update
+        preparedStatement.executeUpdate()
+
+
+
+        //comprueba si se ha realizado el insert
+        if (insertCount > 0) {
+            println("Procesado: Añadida participación del grupo ${ctf.grupoId} en el ${ctf.id} con una puntuacion de ${ctf.puntuacion} puntos.")
+        } else {
+            println("No procesado: La inserción de fila ha fallado.")
+        }
+
+        preparedStatement.close()
+
+    }catch (ex: SQLException) {
+        println(ex.message);
+
     }
 
-    preparedStatement.close()
+
+
 }
-
-
-
 
 // 2º comando elimina participacion de un grupo (insert en ctf) consulta sobre ctf con mejor puntucion de ese grupo
 //(update) de mejor posicion
 
+fun eliminaParticipacion(grupoid: Int,ctfid:Int){
+
+    try{
+        //conexion con la BBDD
+        val conn = DriverManager.getConnection(jdbcUrl, userName, password)
+
+        //actualiza el campo que es clave foranea y no not null a null
+        var updateStatement = ("UPDATE GRUPOS SET mejorposCTFid=NULL where grupoid=?;")
+
+        var preparedStatement = conn.prepareStatement(updateStatement) // Uso de PreparedStatement
+
+        preparedStatement.setInt(1, grupoid)
+
+        //ejecuta el delete
+        preparedStatement.executeUpdate()
+
+
+
+        //ELIMINA LA PARTICIPACION DE UN GRUPO EN EL ctf
+        val deleteSqlString = ("DELETE FROM CTFS WHERE grupoid=? AND ctfid=?")
+
+
+
+        preparedStatement = conn.prepareStatement(deleteSqlString) // Uso de PreparedStatement
+
+        preparedStatement.setInt(1, grupoid)
+        preparedStatement.setInt(2, ctfid)
+
+
+        //ejecuta el delete y cuenta cuantos
+        val deleteCount = preparedStatement.executeUpdate()
+
+
+
+        //Y RECALCULA EL CAMPO MEJORPOS DE LOS GRUPOS EN LA TABLA GRUPOS
+        val updateSqlString = ("UPDATE GRUPOS\n" +
+                "SET mejorposCTFid = (\n" +
+                "  select ctfid\n" +
+                "  from ctfs\n" +
+                "  where grupoid=? and puntuacion=(\n" +
+                "    select max(puntuacion)\n" +
+                "    from CTFS\n" +
+                "    where grupoid=?\n" +
+                "  )\n" +
+                "  limit 1\n" +
+                ")\n" +
+                "where grupoid=?;")
+
+
+        preparedStatement = conn.prepareStatement(updateSqlString) // Uso de PreparedStatement
+
+        preparedStatement.setInt(1, grupoid)
+        preparedStatement.setInt(2, grupoid)
+        preparedStatement.setInt(3, grupoid)
+
+        //ejecuta el update
+        preparedStatement.executeUpdate()
+
+
+
+
+        //comprueba si se ha realizado el delete
+        if (deleteCount > 0) {
+            println("Procesado: Eliminada participación del grupo $grupoid en el CTF $ctfid.")
+        } else {
+            println("No procesado: La eliminación de la fila ha fallado.")
+        }
+    preparedStatement.close()
+
+    }catch (ex: SQLException) {
+    println(ex.message);
+
+    }
+
+}
+
+
 // 3º select del grupo dado, sino info de todo
+fun muestraParticipacion(grupoid:Int){
+
+    //conexion con la BBDD
+    val conn = DriverManager.getConnection(jdbcUrl, userName, password)
+
+
+    val stmt: Statement = conn.createStatement()
+    val rs: ResultSet = stmt.executeQuery("select * from grupos where grupoid=?;")
+
+    if (rs.wasNull()) {
+        //poner condicion de que se imprima entera
+        val lastName = rs.getString("grupoid")
+        println(lastName)
+    }
+
+
+
+    //select del grupo dado
+    var updateStatement = ("select grupoid,grupodesc,mejorposCTFid\n" +
+                            "from grupos\n" +
+                            "where grupoid =?;")
+
+    var preparedStatement = conn.prepareStatement(updateStatement) // Uso de PreparedStatement
+
+    preparedStatement.setInt(1, grupoid)
+
+    //ejecuta el select
+    preparedStatement.executeUpdate()
+
+
+
+}
+
+
+
+
+
 
 
 
